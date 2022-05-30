31a371f27e61ff4feecdb553e5db12e8a134f0f9
iHiram <33489236+iHiram@users.noreply.github.com>
2022-05-30 16:46:56 -0500

    Se agrago el login con auth0

--- public/Controller/convert.js
+++ public/Controller/convert.js
@@ -3,6 +3,94 @@ var ArrayJSONXML = [];
 var ArrayObjToSend = [];
 var mapLabelApiname = {};
 var pathFilterobj='';
+let auth0 = null;
+
+
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+
+
+
+const updateUI = async () => { 
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+  
+  // NEW - add logic to show/hide gated content after authentication
+  if (isAuthenticated) {
+    document.getElementById("gated-content").classList.remove("hidden");
+
+    document.getElementById(
+      "ipt-access-token"
+    ).innerHTML = await auth0.getTokenSilently();
+
+    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+      await auth0.getUser()
+    );
+
+  } else {
+    document.getElementById("gated-content").classList.add("hidden");
+  }
+};
+
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+};
+
+
+const logout = () => {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+};
+
+window.onload = async () => {
+
+  // .. code ommited for brevity
+
+  updateUI();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
+
+
+
+
+
+
+
+
 var p1 = new Promise(async function (resolve, reject) {
   var pth = window.location.href;
   var url = pth + 'Data/Objetos';
@@ -239,3 +327,4 @@ console.log(result)
   }); 
 
 }
+
